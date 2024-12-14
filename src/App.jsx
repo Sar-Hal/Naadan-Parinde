27715027504a4d30773ac4e432fae041288bb1b5
VanXodus305 <soumyadeep.30505@gmail.com>
2024-12-15 01:44:17 +0530

    Added API support

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,27 @@
 import { useState } from "react";
-import axios from "axios";
+import fetch from "cross-fetch";
 import { IoArrowBack } from "react-icons/io5";
 import { FaArrowRight } from "react-icons/fa";
 
-import resourceData from "./constants/resourceData.json";
 import District from "./components/district";
 import DistrictList from "./components/districtList";
+// import resourceData from "./constants/resourceData.json";
 
 const App = () => {
   const [loggedIn, setLoggedIn] = useState(false);
   const [resource, setResource] = useState(undefined);
 
-  // const fetchData = async () => {
-  //   let config = {
-  //     method: "get",
-  //     maxBodyLength: Infinity,
-  //     url: "https://api.mlsakiit.com/resources",
-  //     headers: {},
-  //   };
+  const [resourceData, setResourceData] = useState([]);
 
-  //   axios
-  //     .request(config)
-  //     .then((response) => {
-  //       console.log(JSON.stringify(response.data));
-  //     })
-  //     .catch((error) => {
-  //       console.log(error);
-  //     });
-  // };
-  // fetchData();
+  const fetchData = async () => {
+    const result = await fetch("https://api.mlsakiit.com/resources", {
+      method: "GET",
+    }).catch((err) => console.error(err));
+    const data = await result.json().catch((err) => console.error(err));
+    console.log(data);
+    setResourceData(data.features);
+  };
+  fetchData();
 
   return (
     <>
@@ -38,29 +31,29 @@ const App = () => {
             className={`absolute inset-0 -z-9 h-full w-full items-center px-5 py-24 bg-[url('https://images6.alphacoders.com/856/856983.jpg')] bg-cover`}
           ></div>
         </div>
-        <div className="container mx-auto px-5 py-20 h-screen">
+        <div className="container mx-auto px-5 py-20 h-screen select-none tracking-wider">
           {loggedIn ? (
             <div className="pb-20">
               {resource ? (
                 <div className="flex flex-row gap-3 md:gap-5 items-center justify-start mb-10">
                   <IoArrowBack
-                    className="md:text-5xl text-3xl cursor-pointer mr-2"
+                    className="md:text-6xl text-4xl cursor-pointer mr-2"
                     onClick={() => setResource(undefined)}
                   />
 
-                  <h1 className="md:text-5xl text-3xl font-bold">
-                    {resource.properties.dist_name}
+                  <h1 className="md:text-6xl text-4xl font-bold">
+                    {`${resource.properties.dist_name} [#${resource.properties.dist_code}]`}
                   </h1>
                 </div>
               ) : (
-                <h1 className="md:text-5xl text-3xl mb-7 font-bold">
+                <h1 className="md:text-6xl text-4xl mb-7 font-bold">
                   Resource Districts
                 </h1>
               )}
 
               {resource ? (
                 <div className="w-full flex flex-col gap-10 items-start justify-center">
-                  <District resource={resource.properties} />
+                  <District district={resource.properties} />
                 </div>
               ) : (
                 <div className="flex flex-col gap-8 items-center justify-center pt-6">
@@ -83,7 +76,7 @@ const App = () => {
                   className="py-4 px-8 rounded-2xl bg-[#653504] hover:scale-105 transition-all ease-in-out duration-300 cursor-pointer shadow-lg shadow-[#da9046] flex flex-row gap-4 items-center justify-center"
                   onClick={() => setLoggedIn(true)}
                 >
-                  <h1 className="text-4xl font-bold text-center">
+                  <h1 className="text-5xl font-bold text-center">
                     VIEW DISTRICTS
                   </h1>
                   <FaArrowRight className="text-4xl" />
--- src/components/District.jsx
+++ src/components/District.jsx
@@ -1,7 +1,76 @@
-import React from "react";
-
 const District = ({ district }) => {
-  return <div>district</div>;
+  return (
+    <div className="w-full p-10 bg-[#653504] flex flex-col gap-10 items-center justify-center rounded-2xl border-3 border-[#da9046] shadow-lg shadow-[#80471c]">
+      <div className="flex flex-row gap-12 w-full items-center justify-evenly">
+        <div className="flex flex-col gap-3 items-center justify-center w-full">
+          <h1 className="text-2xl font-bold">Temperature</h1>
+          <div className="flex items-center justify-center w-full rounded-xl bg-[#da9046] p-4 shadow-lg shadow-[#653504] hover:scale-105 transition-all ease-in-out duration-200">
+            <h1 className="font-bold text-2xl text-[#4f3317]">
+              {district.temp}
+            </h1>
+          </div>
+        </div>
+        <div className="flex flex-col gap-3 items-center justify-center w-full">
+          <h1 className="text-2xl font-bold">Rainfall</h1>
+          <div className="flex items-center justify-center rounded-xl bg-[#da9046] p-4 w-full shadow-lg shadow-[#653504] hover:scale-105 transition-all ease-in-out duration-200">
+            <h1 className="font-bold text-2xl text-[#4f3317]">
+              {district.rainfall}
+            </h1>
+          </div>
+        </div>
+        <div className="flex flex-col gap-3 items-center justify-center w-full">
+          <h1 className="text-2xl font-bold">Wind Speed</h1>
+          <div className="flex items-center justify-center rounded-xl bg-[#da9046] p-4 w-full shadow-lg shadow-[#653504] hover:scale-105 transition-all ease-in-out duration-200">
+            <h1 className="font-bold text-2xl text-[#4f3317]">
+              {district.wind_speed}
+            </h1>
+          </div>
+        </div>
+        <div className="flex flex-col gap-3 items-center justify-center w-full">
+          <h1 className="text-2xl font-bold">Water</h1>
+          <div className="flex items-center justify-center rounded-xl bg-[#da9046] p-4 w-full shadow-lg shadow-[#653504] hover:scale-105 transition-all ease-in-out duration-200">
+            <h1 className="font-bold text-2xl text-[#4f3317]">
+              {district.water}
+            </h1>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-row gap-12 w-full items-center justify-evenly">
+        <div className="flex flex-col gap-3 items-center justify-center w-full">
+          <h1 className="text-2xl font-bold">Food Rations</h1>
+          <div className="flex items-center justify-center w-full rounded-xl bg-[#da9046] p-4 shadow-lg shadow-[#653504] hover:scale-105 transition-all ease-in-out duration-200">
+            <h1 className="font-bold text-2xl text-[#4f3317]">
+              {district.food_rations}
+            </h1>
+          </div>
+        </div>
+        <div className="flex flex-col gap-3 items-center justify-center w-full">
+          <h1 className="text-2xl font-bold">MedKits</h1>
+          <div className="flex items-center justify-center rounded-xl bg-[#da9046] p-4 w-full shadow-lg shadow-[#653504] hover:scale-105 transition-all ease-in-out duration-200">
+            <h1 className="font-bold text-2xl text-[#4f3317]">
+              {district.medkits}
+            </h1>
+          </div>
+        </div>
+        <div className="flex flex-col gap-3 items-center justify-center w-full">
+          <h1 className="text-2xl font-bold">Ammo</h1>
+          <div className="flex items-center justify-center rounded-xl bg-[#da9046] p-4 w-full shadow-lg shadow-[#653504] hover:scale-105 transition-all ease-in-out duration-200">
+            <h1 className="font-bold text-2xl text-[#4f3317]">
+              {district.ammo}
+            </h1>
+          </div>
+        </div>
+        <div className="flex flex-col gap-3 items-center justify-center w-full">
+          <h1 className="text-2xl font-bold">Camp Exists</h1>
+          <div className="flex items-center justify-center rounded-xl bg-[#da9046] p-4 w-full shadow-lg shadow-[#653504] hover:scale-105 transition-all ease-in-out duration-200">
+            <h1 className="font-bold text-2xl text-[#4f3317]">
+              {district.camp_exists ? "Yes" : "No"}
+            </h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default District;
