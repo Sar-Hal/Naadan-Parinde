b8cba60da86ec374093a53fc62e2319aedc98e55
VanXodus305 <soumyadeep.30505@gmail.com>
2024-12-14 19:13:02 +0530

    Project Setup

---
+++ tailwind.config.mjs
@@ -0,0 +1,21 @@
+const { nextui } = require("@nextui-org/react");
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background_1: "#041D37",
+        background_2: "#083C70",
+        foreground_1: "#2463A2",
+      },
+    },
+  },
+  darkMode: "class",
+  plugins: [nextui()],
+};
